55bf54ee4452a52fbf67a316f2a3bfe2cfae0762
fdeh <fdeh75@gmail.com>
2024-06-27 01:21:09 +0300

    First commit

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+class Amount extends Number {}
+class Result extends Number {}
+class Rate extends Number {}
+
+const convert = (amount: Amount, rate: Rate): Result => {
+  // TODO
+}
+
+const inverseConvert = (result: Result, rate: Rate): Amount => {
+  // TODO
+}
