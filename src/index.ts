7ca8c4487286ad2a83190a63b52aa85bfe696213
fdeh <fdeh75@gmail.com>
2024-06-27 01:30:40 +0300

    Implementation
    update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,28 @@
-class Amount extends Number {}
-class Result extends Number {}
+import {Decimal} from 'decimal.js';
+
+class Amount extends Number {
+    constructor(val: number, readonly exVal?: Decimal) {
+        super(val)
+    }
+}
+class Result extends Number {
+    constructor(val: number, readonly exVal?: Decimal) {
+        super(val)
+    }
+}
 class Rate extends Number {}
 
-const convert = (amount: Amount, rate: Rate): Result => {
-  // TODO
+
+export const convert = (amount: Amount, rate: Rate): Result => {
+    let [ a, r ] = [amount, rate].map(item => new Decimal(item.toString()))
+    a = amount.exVal ?? a
+    let res = a.mul(r)
+    return new Result(+res.toFixed(2), res)
 }
 
-const inverseConvert = (result: Result, rate: Rate): Amount => {
-  // TODO
+export const inverseConvert = (result: Result, rate: Rate): Amount => {
+    let [ _res, r ] = [result, rate].map(item => new Decimal(item.toString()))
+    _res = result.exVal ?? _res
+    const amount = _res.dividedBy(r)
+    return new Amount(+amount.toFixed(2), amount)
 }
